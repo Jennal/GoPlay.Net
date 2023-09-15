a32715ecc9c7703afeba28820521d14fe73db13c
Jennal <jennalcn@gmail.com>
2023-09-16 01:29:52 +0800

    publish nupkg scripts

--- Frameworks/Scripts/build_nupkg.sh
+++ Frameworks/Scripts/build_nupkg.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -x
 DIR=$(dirname "$0")/..
-VERSION="0.1.0"
+VERSION="0.2.0"
 rm -f $DIR/packages/*
 cd $DIR
 dotnet clean
 dotnet build --configuration Release Server.sln -p:Version="$VERSION"
 dotnet pack --no-build --configuration Release --output packages Server.sln -p:Version="$VERSION"
-#dotnet nuget push packages/GoPlay.Tools.1.0.0.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
---
+++ Frameworks/Scripts/publish_nupkg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+DIR=$(dirname "$0")/..
+VERSION="0.2.0"
+cd $DIR
+
+# Push nupkg
+for f in `ls packages/GoPlay.*$VERSION*.nupkg`; do
+    dotnet nuget push $f --skip-duplicate --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json    
+done
+
+# Push snupkg
+for f in `ls packages/GoPlay.*$VERSION*.snupkg`; do
+    dotnet nuget push $f --skip-duplicate --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json    
+done
