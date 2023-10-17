f9510858186839d32987d91d0842f3d9c9b6b294
Jennal <jennalcn@gmail.com>
2023-10-17 19:49:42 +0800

    fix tool namespace

--- Tools/publish_nuget.sh
+++ Tools/publish_nuget.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 DIR=$(dirname "$0")
-VERSION="1.0.0"
+VERSION="1.0.1"
 rm -f $DIR/packages/*
 dotnet clean
 dotnet build --configuration Release $DIR/Tools.sln -p:Version="$VERSION"
 dotnet pack --no-build --configuration Release --output $DIR/packages $DIR/Tools.sln -p:Version="$VERSION"
-#dotnet nuget push packages/GoPlay.Tools.$VERSION.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push packages/GoPlay.Tools.$VERSION.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
