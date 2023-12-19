35c0a98c863b22826dfd6566ca15226765547b54
Jennal <jennalcn@gmail.com>
2023-12-19 17:29:34 +0800

    fix tool publish script

--- Tools/publish_nuget.sh
+++ Tools/publish_nuget.sh
@@ -5,4 +5,4 @@ rm -f $DIR/packages/*
 dotnet clean
 dotnet build --configuration Release $DIR/Tools.sln -p:Version="$VERSION"
 dotnet pack --no-build --configuration Release --output $DIR/packages $DIR/Tools.sln -p:Version="$VERSION"
-dotnet nuget push packages/GoPlay.Tools.$VERSION.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push $DIR/packages/GoPlay.Tools.$VERSION.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
