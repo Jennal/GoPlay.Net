614048cce681319a6d4494d33a4c6768e1b45b46
jennal <jennalcn@gmail.com>
2024-05-17 23:13:07 +0800

    update to 0.2.10

--- Frameworks/Scripts/build_nupkg.sh
+++ Frameworks/Scripts/build_nupkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 DIR=$(dirname "$0")/..
 PROJ="Server.sln"
-VERSION="0.2.9"
+VERSION="0.2.10"
 rm -f $DIR/packages/*
 cd $DIR
 
--- Frameworks/Scripts/publish_nupkg.sh
+++ Frameworks/Scripts/publish_nupkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 DIR=$(dirname "$0")/..
-VERSION="0.2.9"
+VERSION="0.2.10"
 cd $DIR
 
 # Push nupkg
