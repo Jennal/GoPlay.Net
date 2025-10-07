c6a7dd9f1c2da7dde643fcaf404e603835f7c439
Jennal <jennalcn@gmail.com>
2025-10-07 11:45:31 +0800

    fix IdLoopGenerator thread safe issue

--- Frameworks/Core/Utils/IdLoopGenerator.cs
+++ Frameworks/Core/Utils/IdLoopGenerator.cs
@@ -1,11 +1,14 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
+
 namespace GoPlay.Core.Protocols
 {
     public class IdLoopGenerator
     {
         public const uint INVALID = 0;
-        
+
         protected uint m_current = 1;
-        protected readonly uint m_max = byte.MaxValue;
+        protected readonly uint m_max = uint.MaxValue;
 
         public IdLoopGenerator()
         {
@@ -15,14 +18,24 @@ namespace GoPlay.Core.Protocols
         {
             m_max = maxVal;
         }
-        
+
         public uint Next()
         {
-            var result = m_current;
-            if (m_current >= m_max) m_current = 1;
-            else m_current++;
+            while (true)
+            {
+                var current = Volatile.Read(ref m_current);
+                var next = current >= m_max ? 1u : current + 1u;
 
-            return result;
+                var original = (uint)Interlocked.CompareExchange(
+                    ref Unsafe.As<uint, int>(ref m_current),
+                    (int)next,
+                    (int)current);
+
+                if (original == current)
+                {
+                    return current;
+                }
+            }
         }
 
         public void Reset()
